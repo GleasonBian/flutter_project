c16c4109096b971bc9f27c769e95c8f526d03d9b
Gleason <bianliuzhu@gmail.com>
2020-06-27 12:17:21 +0800

    使用 future builder 处理 请求中文乱码

--- lib/main.dart
+++ lib/main.dart
@@ -15,37 +15,61 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String showResult = '';
-  Future<CommonModel>fetchPost() async{
-    var response = await http.get('https://www.devio.org/io/flutter_app/json/test_common_model.json');
+
+  Future<CommonModel> fetchPost() async {
+    var response = await http.get(
+        'https://www.devio.org/io/flutter_app/json/test_common_model.json');
+    // 处理中文乱码
+    Utf8Decoder utf8decoder = Utf8Decoder();
+    final result = json.decode(utf8decoder.convert(response.bodyBytes));
+
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
-    final result = json.decode(response.body);
     return CommonModel.fromJson(result);
-
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('http'),
+          title: Text('future 与 futureBuilder 实用技巧'),
         ),
-        body: Column(
-          children: [
-            // 可点击的按钮
-            InkWell(
-              onTap: (){
-                fetchPost().then((CommonModel value){
-                  setState(() {
-                    showResult = '请求结果: \n hideAppBar: ${value.hideAppBar} \n icon: ${value.icon}';
-                  });
-                });
-              },
-              child: Text('点我',style: TextStyle(fontSize: 26),),
-            ),
-            Text(showResult)
-          ],
-        ),
+        // ignore: missing_return
+        body: FutureBuilder<CommonModel>(
+            // future 参数切记
+            future: fetchPost(),
+            builder:
+                // ignore: missing_return
+                (BuildContext context, AsyncSnapshot<CommonModel> snapshot) {
+              // ignore: missing_return
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return Text('input a url to start');
+                case ConnectionState.waiting:
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                case ConnectionState.active:
+                  return Text('active');
+                case ConnectionState.done:
+                  if (snapshot.hasError) {
+                    return Text(
+                      '${snapshot.error}',
+                      style: TextStyle(color: Colors.red),
+                    );
+                  } else {
+                    return Column(
+                      children: [
+                        Text('icon:${snapshot.data.icon}'),
+                        Text('statusBarColor:${snapshot.data.statusBarColor}'),
+                        Text('title: ${snapshot.data.title}'),
+                        Text('title: ${snapshot.data.url}'),
+                      ],
+                    );
+                  }
+              }
+            }),
       ),
     );
   }
@@ -60,10 +84,11 @@ class CommonModel {
   final bool hideAppBar;
 
   // 定义构造方法 参数可选
-  CommonModel({this.icon, this.title, this.url, this.statusBarColor, this.hideAppBar});
+  CommonModel(
+      {this.icon, this.title, this.url, this.statusBarColor, this.hideAppBar});
 
   // 创建工厂方法factory fromJson 参数为 动态类型 map
-  factory CommonModel.fromJson(Map<String,dynamic>json){
+  factory CommonModel.fromJson(Map<String, dynamic> json) {
     return CommonModel(
       icon: json['icon'],
       title: json['title'],
